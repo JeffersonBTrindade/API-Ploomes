c791db80ceb157b4a967e123a695835fbe84d1c6
JeffersonBrits <jeffersonbtrindade@gmail.com>
2021-01-31 21:52:29 -0300

    primeiro commit, sem InteractionRecords

---
+++ testeAPIPloomes/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using RestSharp;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System.Net.Http;
+using System.Net.Http.Formatting;
+
+namespace testeAPIPloomes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            JObject novoCliente = new JObject();
+            novoCliente.Add("Name", "Jefferson Test");
+            novoCliente.Add("TypeId", 2);
+
+            JArray cliente = RequestHandler.MakePloomesRequest($"Contacts", RestSharp.Method.POST, novoCliente);
+
+            JObject novaNegociacao = new JObject();
+            novaNegociacao.Add("Title", "Nova Negociação");
+            novaNegociacao.Add("ContactId", cliente[0]["Id"].ToString());
+
+            JArray negociacao = RequestHandler.MakePloomesRequest($"Deals", RestSharp.Method.POST, novaNegociacao);
+
+            JObject novaTarefa = new JObject();
+            novaTarefa.Add("Title", "Tarefa 1");
+            novaTarefa.Add("Description", "Tarefa da Nova Negocição");
+            novaTarefa.Add("ContactId", cliente[0]["Id"].ToString());
+            novaTarefa.Add("DealId", negociacao[0]["Id"]);
+
+            JArray tarefa = RequestHandler.MakePloomesRequest($"Tasks", RestSharp.Method.POST, novaTarefa);
+
+            JObject patchNegociacao = new JObject();
+            patchNegociacao.Add("Amount", "15000");
+
+            JArray negociacaoPatch = RequestHandler.MakePloomesRequest($"Deals({(int)negociacao[0]["Id"]})", RestSharp.Method.PATCH, patchNegociacao);
+
+            JArray tarefaTerminada = RequestHandler.MakePloomesRequest($"Tasks({tarefa[0]["Id"]})/Finish", RestSharp.Method.POST);
+
+            JArray negociacaoGanha = RequestHandler.MakePloomesRequest($"Deals({negociacao[0]["Id"]})/Win", RestSharp.Method.POST);
+
+            JObject desHistoricoCliente = new JObject();
+            desHistoricoCliente.Add("Content", "Negócio Fechado!");
+
+            JArray historicoCliente = RequestHandler.MakePloomesRequest($"InteractionRecords?$filter=ContactId+eq+{cliente[0]["Id"]}", RestSharp.Method.POST, desHistoricoCliente);
+
+            Console.WriteLine(negociacao.ToString());
+            Console.WriteLine(tarefa.ToString() + "\n\n");
+            Console.WriteLine(negociacaoPatch.ToString() + "\n\n");
+            Console.WriteLine(tarefaTerminada.ToString() + "\n\n");
+            Console.WriteLine(negociacaoGanha.ToString() + "\n\n");
+
+           // JArray historico = RequestHandler.MakePloomesRequest($"InteractionRecords?$filter=ContactId+eq+{cliente[0]["Id"]}", RestSharp.Method.GET);
+            Console.WriteLine(historicoCliente.ToString() + "\n\n");
+
+
+
+
+
+        }
+    }
+}
---
+++ testeAPIPloomes/RequestHandler.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using RestSharp;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System.Net.Http;
+using System.Net.Http.Formatting;
+
+namespace testeAPIPloomes
+{
+    class RequestHandler
+    {
+        private static string PLOOMES_API_PATH = "https://api2.ploomes.com/";
+        private static HttpClient ploomesClient;
+        private static string userkey = "66907FAF34000610221422A600AF0B07EC777D3AE18FCBA4AC8A1D41AC0F7839D87AAF5AB10FD5D5C106FD60814CB189532E6A7D0459BEEC0EB0E808916C4335";
+
+        public static void instantiatePloomesConnection()
+        {
+            ploomesClient = new HttpClient();
+            ploomesClient.DefaultRequestHeaders.Add("User-key", userkey);
+            ploomesClient.DefaultRequestHeaders.Accept.Add(new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("aplication/json"));
+        }
+
+        public static JArray MakePloomesRequest(string url, Method method, JObject json = null)
+        {
+            try
+            {
+                instantiatePloomesConnection();
+
+                System.Threading.Thread.Sleep(1000);
+                string response = string.Empty;
+                url = PLOOMES_API_PATH + url;
+
+                if (method == Method.GET)
+                    response = ploomesClient.GetAsync(url).Result.Content.ReadAsStringAsync().Result;
+                else if (method == Method.POST)
+                {
+                    if (json != null)
+                        response = ploomesClient.PostAsync(url, new StringContent(json.ToString())).Result.Content.ReadAsStringAsync().Result;
+                    else
+                        response = ploomesClient.PostAsync(url, new StringContent(new JObject().ToString())).Result.Content.ReadAsStringAsync().Result;
+                }
+                else if (method == Method.PATCH)
+                {
+                    var content = new ObjectContent<JObject>(json, new JsonMediaTypeFormatter());
+                    var request = new HttpRequestMessage(new HttpMethod("PATCH"), url) { Content = content };
+                    response = ploomesClient.SendAsync(request).Result.Content.ReadAsStringAsync().Result;
+                }
+                else if (method == Method.DELETE)
+                {
+                    ploomesClient.DeleteAsync(url);
+                    return null;
+                }
+
+                return JsonConvert.DeserializeObject<JObject>(response)["value"] as JArray;
+            }
+
+            catch (Exception ex)
+            {
+                Console.Error.WriteLine("Error in MakePloomesRequest method --> " + ex.Message);
+                throw ex;
+            }
+        }
+    }
+}
